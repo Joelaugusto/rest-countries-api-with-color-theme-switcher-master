2daca4dce6ececdd805fb0eb7d94b5e240abe02e
Augusto <joelaugusto97@gmail.com>
2021-03-07 02:36:52 +0200

    dark mode

--- src/App.vue
+++ src/App.vue
@@ -12,15 +12,34 @@ body {
   margin: 0 auto;
 }
 
-/*:root {
-  color-scheme: light dark; /* both supported *//*
-}*/
 
-.root {
-  --background: hsl(0, 0%, 98%);
+*{
+    outline-style: none;
+    box-shadow: none;
+    border-color: transparent;
+}
+/*:root {
+  color-scheme: light dark; /* both supported */ /*
+}*/
+body {
+  --background:#FFFFFF;
+  --second-background: #FAFAFA;
   --element: hsl(0, 0%, 100%);
   --text: hsl(200, 15%, 8%);
+  --shadow: #8080803d;
 }
+body.dark {
+  --background: hsl(207, 26%, 17%);
+  --second-background: #2B3743;
+  --element: hsl(209, 23%, 22%);
+  --text: hsl(0, 0%, 100%);
+  --shadow: #8080803d;
+}
+body{
+  background: var(--background);
+  margin: 0 auto;
+}
+
 /*:-moz-drag-over
 [data-theme="dark"] {
   --background: hsl(207, 26%, 17%);
